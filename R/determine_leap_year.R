bff4ccd66568a030018f8a30df84ca34cbd40f57
TheLeopards <georgios.anastasiou@wur.nl>
2016-01-07 17:07:29 +0100

    Moved function from main.R to R folder

--- R/determine_leap_year.R
+++ R/determine_leap_year.R
@@ -3,7 +3,6 @@
 
 #This function was created to determine whether a year is a leap year.
 
-print ("Is the following year a leap year?")
 is.leap <- function(x) {
 	# check whether "x" is a number
 	if(!is.numeric(x)) {
@@ -11,22 +10,23 @@ is.leap <- function(x) {
 	} 
 	# check whether "x"is an integer
 	else if (x %%1 != 0) {
-		stop ("argument is not an integer", call.=FALSE)
+		stop (paste(x, "is not an integer"), call.=FALSE)
 	} 
 	# check whether "x" is a year after the introduction
 	# of the Gregorian calendar	
 	else if (x <= 1582) {
-		stop ("argument is out of the valid range", call.=FALSE)	
+		return (paste(x, "is out of the valid range"))	
 	} 
 	# check whether "x" is a leap year
 	else if (x %% 4 != 0) {
-		stop ("False")
+		common <- x %% 4 == 0
+		return (common)
 	} else if (x %% 100 != 0) {
-		stop("true")
+		leap <- x%%100 != 0
+		return(leap)
 	} else if (x %% 400 != 0) {
-		stop ("false2")
+		common <- x %% 400 == 0
+		return (common)
 	} 
-	return("TRUE")
+	return(TRUE)
 }
-
-is.leap(2004)
--- main.R
+++ main.R
@@ -3,32 +3,4 @@
 
 #This function was created to determine whether a year is a leap year.
 
-is.leap <- function(x) {
-		# check whether "x" is a number
-	if(!is.numeric(x)) {
-		stop("argument of class numeric expected", call.=FALSE)
-	} 
-		# check whether "x"is an integer
-		else if (x %%1 != 0) {
-			stop (paste(x, "is not an integer"), call.=FALSE)
-	} 
-		# check whether "x" is a year after the introduction
-		# of the Gregorian calendar	
-		else if (x <= 1582) {
-			return (paste(x, "is out of the valid range"))	
-	} 
-		# check whether "x" is a leap year
-		else if (x %% 4 != 0) {
-			common <- x %% 4 == 0
-			return (common)
-	} else if (x %% 100 != 0) {
-			leap <- x%%100 != 0
-			return(leap)
-	} else if (x %% 400 != 0) {
-			common <- x %% 400 == 0
-			return (common)
-	} 
-	return(TRUE)
-}
-
-is.leap(2500.5)
+is.leap(2000)
